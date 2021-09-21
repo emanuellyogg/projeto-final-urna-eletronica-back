d4308d96e55d1c3741569fb1e0ec295cb373a315
Bruna Neto Bitencourt <bruna.bitencourt@capgemini.com>
2021-09-21 17:23:35 -0300

    Validação de voto repetido

--- server.ts
+++ server.ts
@@ -34,22 +34,49 @@ app.post("/voto", verificaVoto, function(req,resp){
 })
 
 //Função que vai verificar se o voto é repetido ou não e se está dentro do período de votação
-function verificaVoto(req, resp, next){
-    //let votoUnico:boolean = verificaRepetido(req.body.eleitor)
-    //let votoNoPrazo:boolean = verificaPrazoVoto(req.body.timestamp)
-    /*if(votoUnico && votoNoPrazo){
-        next()
-    }else{
-        if(!votoUnico){
-            return resp.json({
-                "status": "401",
-                "mensagem": "Você só pode votar uma vez"
-            })
+async function verificaVoto(req, resp, next){
+    let verificaVoto1 = await verificaRepetido(req.body.eleitor)
+    console.log(verificaVoto1);
+    
+    //let verificaVoto2 = await verificaPrazoVoto(req.body.timestamp)
+    if(verificaVoto1.validacao){ //&& verificaVoto2.validacao){
+        if(verificaVoto1.naoRepete){//&& verificaVoto2.validacao){
+            next()
         }else{
-            return resp.json({
-                "status": "401",
-                "mensagem": "Voto fora do período de votação"
-            })            
+            if(!verificaVoto1.naoRepete){
+                return resp.json({
+                    "status": "401",
+                    "mensagem": "Você só pode votar uma vez"
+                })
+            }else{
+                return resp.json({
+                    "status": "401",
+                    "mensagem": "Voto fora do período de votação"
+                })            
+            }
         }
-    }*/
+    }else{
+        return resp.json({
+            "status": "500",
+            "mensagem": "Erro ao ler arquivo"
+        })  
+    }
+}
+
+async function verificaRepetido(eleitor){
+    try{
+        let naoRepete = true
+        let data = await fsPromises.readFile("votos.csv", "utf-8")
+        let dados = data.split("\n")
+        for(let i=0; i < dados.length; i++){
+            let voto = dados[i].split(";")
+            if(voto[0] == eleitor){
+                naoRepete = false
+            }
+        }
+        return {validacao: true, naoRepete: naoRepete}
+    }catch(err){
+        console.log(err);        
+        return {validacao: false}
+    }
 }
