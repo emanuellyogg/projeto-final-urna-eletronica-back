2c5f2abd0654e7280f34f7e54f9c2491e75018fe
Emanuelly Maria Ogg Vicente <emanuelly.vicente@capgemini.com>
2021-09-21 18:23:12 -0300

    feat: validação de usuário

--- server.js
+++ server.js
@@ -11,7 +11,30 @@ var porta = 3001;
 app.listen(porta, function () {
     console.log("Servidor rodando na porta " + porta);
 });
+// Rota que busca validar o usuário logado
 app.get("/validaUsuario", function (req, res) {
-    var data = (0, fs_1.readFileSync)("./opcoes_usuarios/usuariosRG.csv", "utf-8");
-    res.json({ data: data });
+    var data = (0, fs_1.readFileSync)("./opcoes_usuarios/usuarios.csv", "utf-8");
+    var listaUser = [];
+    listaUser.push(data.split(";"));
+    // número do CPF digitado
+    var user = req.body.nmUser;
+    console.log(typeof listaUser);
+    // verificador de user valido ou inválido
+    var isValid = false;
+    for (var i = 0; i < listaUser[0].length - 1; i++) {
+        var element = listaUser[0][i];
+        if (user == element) {
+            console.log("validado");
+            isValid = true;
+            break;
+        }
+    }
+    console.log(isValid);
+    // retorno para o frontend
+    if (isValid) {
+        res.send(true);
+    }
+    else {
+        res.send(false);
+    }
 });
--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import * as fsPromises from "fs/promises";
 import * as jwt from "jsonwebtoken";
 import * as path from "path";
 import * as cors from "cors";
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 const app = express();
 
@@ -18,8 +18,34 @@ app.listen(porta, function () {
   console.log("Servidor rodando na porta " + porta);
 });
 
-app.get("/validaUsuario",  function (req, res) {
-  const data = readFileSync("./opcoes_usuarios/usuariosRG.csv", "utf-8");
-  res.json({ data })
-});
+// Rota que busca validar o usuário logado
+app.get("/validaUsuario", function (req, res) {
+  const data = readFileSync("./opcoes_usuarios/usuarios.csv", "utf-8");
+  let listaUser = [];
 
+  listaUser.push(data.split(";"));
+
+  // número do CPF digitado
+  let user = req.body.nmUser; 
+  console.log(typeof listaUser);
+
+  // verificador de user valido ou inválido
+  let isValid = false;
+
+  for (let i = 0; i < listaUser[0].length - 1; i++) {
+    const element = listaUser[0][i];
+    
+    if (user == element) {
+      isValid = true;
+      break
+    } 
+  }
+    
+  // retorno para o frontend
+  if (isValid) {
+    res.send(true)
+    
+  } else {
+    res.send(false)
+  }
+});
